9265a640745633c78861c40f8ffbb5aed2cc67da
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-10 14:54:23 +0200

    add tailwind and configure prettier

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script lang="ts" setup>
-import { RouterView } from "vue-router";
+import { RouterView } from 'vue-router';
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*{.vue,.js,.css}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
