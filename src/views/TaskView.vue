ad787a5097c8971490e7ffd3ece7912a43c0954e
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-11 11:36:30 +0200

    add upd/create functionality

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -2,15 +2,35 @@
   <div class="bg" @click.self="router.push({ name: 'board' })">
     <div v-if="task" class="task-view">
       <div class="task-header">
-        {{ task.name }}
+        <div>
+          <input
+            type="text"
+            v-model="name.value.value"
+            class="text-xl p-2 outline-none"
+            @change="onChange('name', name.value.value)"
+          />
+        </div>
+      </div>
+      <div>
+        <div class="mt-2"></div>
+          <textarea
+            v-model="description.value.value"
+            class="w-full resize-none p-2 outline-none h-20"
+            placeholder="Add description"
+            @blur="onChange('description', description.value.value)"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useBoard } from '@/stores';
 import { useRoute, useRouter } from 'vue-router';
+import { useField, type FieldContext } from 'vee-validate';
+import * as yup from 'yup';
+import { useBoard } from '@/stores';
+import type { Task } from '@/types';
 
 const router = useRouter();
 const route = useRoute();
@@ -25,6 +45,24 @@ const task = board.getTaskById(route.params.id as string);
 if (!task) {
   router.push({ name: 'notFound' });
 }
+
+const name = useField<string>(
+  'name',
+  yup.string().required().min(3).not([task?.name]),
+  { initialValue: task?.name || '' }
+);
+const description = useField<string>('description', undefined, {
+  initialValue: task?.description || 'dawdawdaw',
+});
+
+const fields: Record<string, FieldContext<string>> = { name, description };
+
+const onChange = async (key: string, value: string) => {
+  if (fields[key].errors.value.length !== 0 || !task) {
+    return;
+  }
+  board.updateTask(task, key as keyof Task, value);
+};
 </script>
 
 <style scoped>
@@ -34,11 +72,11 @@ if (!task) {
 }
 
 .task-view {
-  @apply relative flex flex-row bg-white m-32 mx-auto py-4 text-left rounded shadow;
+  @apply relative flex flex-col bg-white m-32 mx-auto p-4 text-left rounded shadow;
   max-width: 700px;
 }
 
 .task-header {
-  @apply flex flex-col flex-grow items-start justify-between px-4;
+  @apply flex flex-col flex-grow items-start justify-between;
 }
 </style>
